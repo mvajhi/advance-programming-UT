322b1f598c7a1eee182fca46bd5b458ebe34e5f6
mahdi vajhi <mvajhimv@gmail.com>
2023-02-08 07:00:44 +0330

    get repeat input

---
+++ hw0/1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (void)
+{
+	int repeat = 0;
+	cin >> repeat;
+	for (int i = 0; i < repeat; i++)
+	{
+		cout << i << ".hi\n";
+	//TODO 
+	}
+
+	return 0;
+}
+	
