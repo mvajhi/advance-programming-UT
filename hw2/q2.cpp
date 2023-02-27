da3367131487e6505a180cd9501b5cc166fcfe4c
mahdi vajhi <mvajhimv@gmail.com>
2023-02-27 23:02:05 +0330

    wq2 remove test log

--- hw2/q2.cpp
+++ hw2/q2.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 void rabbit_method(int &method_count, int matrix_size);
-void move(int &method_count, int matrix_size, vector<int> state, vector<vector<int>> history);
+void move(int &method_count, int matrix_size, vector<int> state);
 
 int main()
 {
@@ -22,32 +22,23 @@ int main()
 
 void rabbit_method(int &method_count, int matrix_size)
 {
-	vector<vector<int>> a;
-	move(method_count, matrix_size, {0, matrix_size}, a);
+	move(method_count, matrix_size, {0, matrix_size});
 	return;
 }
 
-void move(int &method_count, int matrix_size, vector<int> state, vector<vector<int>> history)
+void move(int &method_count, int matrix_size, vector<int> state)
 {
-	history.push_back(state);
 	if (state[0] == matrix_size && state[1] == 0)
 	{
 		method_count++;
-		cout << method_count << ". ";
-		for (int i = 0; i < history.size(); i++)
-			cout << "(" << history[i][0] << ", " << history[i][1] << ") ";
-		cout << endl;
 		return;
 	}
 	else if (state[0] > matrix_size || state[1] < 0 || state[0] + state[1] > matrix_size)
-	{
-		cout << endl;
 		return;
-	}
 	
-	move(method_count, matrix_size, {state[0] + 1, state[1]}, history);
+	move(method_count, matrix_size, {state[0] + 1, state[1]});
 
-	move(method_count, matrix_size, {state[0], state[1] - 1}, history);
+	move(method_count, matrix_size, {state[0], state[1] - 1});
 	
 	return;
 }
