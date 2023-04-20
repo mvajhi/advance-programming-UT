e65cc614ed86be3715dfcc0b12d390582b9b32b8
mohammadamintavanai <tavanaim@gmail.com>
2023-04-20 16:45:34 +0330

    merge

--- hw5/game_board.cpp
+++ hw5/game_board.cpp
@@ -1,6 +1,26 @@
 
 game_board::game_board(/* args */)
 {
+    vetcor<vector<char>> read_board_game(string address_file)
+    {
+        
+        ifstream board_file(address_file);
+        string line;
+        int row_conter=0;
+        vetcor<vector<char>> output;
+        while(getline(board_file,line))
+        {
+            output.push_back({});
+
+        
+            for(int char_counter=0;char_counter<line.size();char_counter++)
+            {
+                output.push_back(line[char_counter]) 
+            }
+
+        }
+    }
+
 }
 
 game_board::~game_board()
