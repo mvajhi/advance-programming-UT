398a1ada5a61b40c1082d39dbd9947d0e239e114
mohammadamintavanai <tavanaim@gmail.com>
2023-04-24 11:33:39 +0330

    merge

--- hw5/turtle.cpp
+++ hw5/turtle.cpp
@@ -1,4 +1,5 @@
 #include "person.hpp"
+#include "turtle.hpp"
 Turtle::Turtle(int x_,int y_,int v_x_,int v_y_,int jump_high_,int health_,int score_)
     :Person(health_,x_,y_,jump_high_,v_x_,v_y_)
 {
