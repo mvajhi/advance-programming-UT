44895f0d34ea70d9b945e5284e4565f226f17b9b
mohammadamintavanai <tavanaim@gmail.com>
2023-04-24 10:54:50 +0330

    merge

---
+++ hw5/person.cpp
@@ -0,0 +1,13 @@
+#include "person.hpp"
+
+Person::Person(int health_,int x_,int y_,int jump_high_,int v_x_,int v_y_)
+{
+    v_x=v_x_;
+    v_y=v_y_;
+    x=x_;
+    y=y_;
+    jump_high=jump_high_;
+    health=health_;
+}
+
+   
--- hw5/person.hpp
+++ hw5/person.hpp
@@ -1,13 +1,23 @@
 #pragma once
-class person
+#include <string>
+#include <vector>
+#include <utility>
+#include "difine.hpp"
+
+using namespace std;
+class Person
 {
+    
+    public:
+    Person(int health_,int x_,int y_,int jump_high_=JUMP_HIGH,int v_x_=0,int v_y_=0);
+    virtual vector<string> move(vector<string> game_board,pair<int ,int> portal_coordinate)=0;
+
     private:
-    float v_x;
-    float v_y;
-    float x;
-    float y;
-    float jump_high;
+    int v_x;
+    int v_y;
+    int x;
+    int y;
+    int jump_high;
     int health;
-    int score;
 
 }
